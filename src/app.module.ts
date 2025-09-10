bf3cbbfac7bcb4ec6a3e56598653c2cbee1a9c9a
Frank Cotera <cmfi1311595928@gmail.com>
2025-09-10 16:10:44 -0500

    Instalcion de GraphQl en Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),],
   controllers: [ ],
   providers: [],
 })
