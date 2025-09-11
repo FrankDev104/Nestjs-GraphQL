aba390b0fe8f18f386f15932de0957511d75b01e
Frank Cotera <cmfi1311595928@gmail.com>
2025-09-11 09:23:14 -0500

    Resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { HellowordModule } from './helloword/helloword.module';
 
 @Module({
   imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),],
+    }), HellowordModule,],
   controllers: [ ],
   providers: [],
 })
